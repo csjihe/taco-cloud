fbbde919799a3236f2e0b0894ab148f02d553833
Ji He <csjihe@gmail.com>
2018-11-04 21:16:43 -0500

    JPA started, Order, Taco and Ingredient annotated with JPA annotations

--- src/main/java/com/example/tacocloud/Ingredient.java
+++ src/main/java/com/example/tacocloud/Ingredient.java
@@ -1,12 +1,21 @@
 package com.example.tacocloud;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
